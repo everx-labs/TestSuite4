f4b85cead7aebb597d267fb11514aa2d93961d51
Vadim <darksteellord@gmail.com>
2022-04-22 15:12:42 +0300

    add dump as json

--- tonos_ts4/dump.py
+++ tonos_ts4/dump.py
@@ -142,3 +142,12 @@ def dump_js_data(path = '.'):
         print('var nicknames = ' + dump_struct_str(globals.NICKNAMES) + ';', file = f)
         print('var allRuns = ' + dump_struct_str(all_runs) + ';', file = f)
 
+def dump_json_data():
+    all_runs = get_all_runs()
+    msgs = get_all_messages()
+    with open('msg_data.json', 'w') as f:
+        json.dump({
+            'allMessages': msgs,
+            'nicknames': globals.NICKNAMES,
+            'allRuns': all_runs,
+        }, indent=2, fp=f)
